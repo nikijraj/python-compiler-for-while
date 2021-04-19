d65ca8aba0f40db1de2c3ef228b4e5814716b278
Sree Pranavi <54464623+SreePranavi@users.noreply.github.com>
2021-04-19 15:05:05 +0530

    Final Code added

---
+++ ip.py
@@ -0,0 +1,47 @@
+#import hWorld
+x=80
+y=10
+
+#Comment1
+#x+y
+#listX = []
+
+c=x*4
+m=x/8
+
+n=x/10
+
+'''
+multi line cmt
+'''
+
+l=m+n
+
+k=m*n
+
+h=m+n
+
+def F1(A, B, C):
+	while(x==True):				#p==True
+		c=0
+		for i in range(x,y):
+			hello="world"
+		z=10
+		b=z
+		w=21
+	return
+
+					#sdbfkdsjbgdjhbgdkf	
+
+def F2():
+	def F3():
+		c=111
+		def F4():
+			d=233
+
+
+
+
+F2()
+#Comment2
+F1(10, 10, 10)
--- makefile
+++ makefile
@@ -1,11 +1,11 @@
 Test.out : lex.yy.c y.tab.c y.tab.h 
 	gcc lex.yy.c y.tab.c -g -lm -lfl -o Test.out
 
-y.tab.c : grammar.y
-	yacc -dv grammar.y
+y.tab.c : python_compiler.y
+	yacc -dv python_compiler.y
 
-lex.yy.c : grammar.l
-	lex grammar.l
+lex.yy.c : python_compiler.l
+	lex python_compiler.l
 
 clean :
 	rm lex.yy.c y.tab.c y.tab.h Test.out y.output
